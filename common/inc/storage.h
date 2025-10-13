03bdd3e038ac7d2c2e0f7f85402abf1074a0f6bc
tearupheyfish <desolationsong@163.com>
2025-10-13 12:05:12 +0800

    非常坏QtKeychain，😇✝

--- common/inc/storage.h
+++ common/inc/storage.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#include "ylt/reflection/member_names.hpp"
+//todo: storage 缓存读取 & 线程锁
+#include <ylt/reflection/member_value.hpp>
 
 #include <fstream>
 #include <algorithm>
-#include <mutex>
+
+#include "storage.h"
 
 /** 存储布局 */
 #pragma pack(push, 1)
@@ -15,7 +17,30 @@
     };
 #pragma pack(pop)
 
-template<typename Layout>
+//能作为字符串模板常量的hack
+template <size_t N>
+struct fixed_string {
+    char data[N];
+    constexpr fixed_string(const char (&str)[N]) {
+        std::copy_n(str, N, data);
+    }
+    operator std::string() const
+    {
+        //指针+长度的构造
+        std::string ret{data, N-1};
+        return ret;
+        return {data, N-1};
+    }
+    constexpr std::string_view view() const noexcept { return {data, N - 1}; }
+    constexpr fixed_string filenamify()const
+    {
+        fixed_string ret = *this;
+        std::replace(ret.data, ret.data+N, ':', '-');
+        return ret;
+    }
+};
+
+template<typename Layout, fixed_string storagename>
 class Storage
 {
 public:
@@ -25,36 +50,44 @@ public:
     bool exist() const;
     //存
     template<auto Member, typename T>
-    bool save(const T& value);
+    void save(T&& value);
     //取
     template<auto Member>
     auto load() const;
     //清空记录
+static
     void clear();
+static
+    void dosave();
 
-    Storage(std::string name = filenameify(std::string(ylt::reflection::get_struct_name<Layout>())))
-        :storage_name(std::move(name))
-    {}
-    ~Storage();
+protected:
 
-private:
-    static std::string filenameify(std::string str)
-    {
-        auto ret = str;
-        std::replace(ret.begin(), ret.end(), ':', '-');
-        return ret;
-    }
-
+    //编译期生成的成员名称数组 和 成员偏移数组
     inline static const auto keys = ylt::reflection::get_member_names<Layout>();
-    const std::string storage_name;
-
-mutable
-        std::mutex _mtx;
+    // inline static const auto offs = ylt::reflection::member_offsets<Layout>();
+    //todo: 读写锁
+    //缓存 和 字段是否为空的记录
+static inline
+    std::array<bool, ylt::reflection::members_count<Layout>()> hascached{};
+static inline
+    Layout cache{};
 #ifndef GUI_BUILD_SINGLE
-mutable
-    std::fstream _file;
+static inline
+    std::fstream _file ;
 #endif
 };
+
+template <class T>
+struct StorageA : Storage<T, []{
+    constexpr auto name_view = ylt::reflection::get_struct_name<T>();
+    constexpr auto N = name_view.size() + 1;
+    char buf[N]{};
+    for (size_t i = 0; i < N - 1; ++i)
+        buf[i] = name_view[i];
+    buf[N - 1] = '\0';
+    return fixed_string(buf);
+}()>
+{};
 
 #ifdef GUI_BUILD_SINGLE
 
@@ -66,65 +99,103 @@ template <typename JobType>
 {
     SyncJob(const QString &service, QObject *parent = nullptr)
     : JobType(service, parent){}
-    /*能写出这样的代码我真是炉火纯青hi啊hi啊hiahia @25.10.3*/
+    /*能写出这样的代码我真是炉火纯青hi啊hi啊hiahia --25.10.3*///不好一点也不好 --25.10.13
     void start(const std::function<void(QKeychain::Job*)>& afterfinish)
     {
         QEventLoop loop;
-        //不会重复注册，qt信号槽自带去重
-        QObject::connect(this, &QKeychain::Job::finished, [&loop, &afterfinish, this]()
+        /* 假设finished信号一定会发，已知它是Direct的
+         *     那么这段lambda函数会立刻执行，将loop标记quit
+         * 在同一个槽函数里，基类会登记它的deleteLater()，不过是Queued的，所以loop还没处理它就quit了
+         * 所以这里应该是健壮的 */
+        QObject::connect(this, &QKeychain::Job::finished, [&loop, &afterfinish, this](QKeychain::Job* job)
             {
+                qDebug()<<"收到Job结束信号";
                 if (afterfinish)
                     afterfinish(this);
                 loop.quit();
             });
+        qDebug()<<"请求Job:"<<this->key();
         JobType::start();
         loop.exec();
     }
 };
 
-template<class Layout>
-    Storage<Layout>::operator bool()const{return true;}
+template<typename Layout, fixed_string storagename>
+    Storage<Layout, storagename>::operator bool()const{return true;}
 
-template<class Layout>
-    bool Storage<Layout>::exist()const
+template<class Layout, fixed_string storagename>
+    bool Storage<Layout, storagename>::exist()const
     {
-        bool keyExists = true;
-        for (auto key : keys) // 遍历 keys 检查存在性
+        bool vkeyexist = true;
+        ylt::reflection::for_each(cache, [&vkeyexist](auto& member, auto key, auto index)
         {
-            SyncJob<QKeychain::ReadPasswordJob> readJobSync(QString::fromUtf8(storage_name));
+            qDebug()<<"准备检查"<<key<<"["<<index<<"]";
+            if (hascached[index] || vkeyexist == false)
+                return;
+            // 超天才QtKeychain，使我刮地三尺（搜罗野指针）😇✝  小半辈子花在这了
+            //
+            // ⚠️警告：要么用指针，要么别在生命周期内eventloop
+            //        仅此一家的擅自deleteLater，不考虑自身是不是栈变量 --25.10.13
+            auto readJobSync = new SyncJob<QKeychain::ReadPasswordJob>{QString::fromStdString(storagename)};
 
-            readJobSync.setKey(QString::fromUtf8(key));
+            readJobSync->setKey(QString::fromUtf8(key));
 
-            readJobSync.start([&keyExists](QKeychain::Job* job)
+            readJobSync->start([&vkeyexist,index,&member](QKeychain::Job* job)
             {
-                if (job->error() != QKeychain::NoError) {
-                    keyExists = false;
+                if (job->error() != QKeychain::NoError)
+                {
+                    vkeyexist = false;
+                    qDebug()<<"job失败:"<<job->errorString();
                 }
+                //顺便把load给做了
+                const auto& data = dynamic_cast<QKeychain::ReadPasswordJob*>(job)->binaryData();
+                memcpy(&member, data.data(), sizeof(member));
+                hascached[index] = true;
             });
+        });
 
-            if (! keyExists)
-                return false;
-        }
-
-        return keyExists;
+        return vkeyexist;
     }
 
-template<class Layout>
-    void Storage<Layout>::clear()
+template<class Layout, fixed_string storagename>
+    void Storage<Layout, storagename>::clear()
     {
         for (auto key : keys)
         {
-            auto eraseJobSync = new SyncJob<QKeychain::DeletePasswordJob>(QString::fromUtf8(storage_name));
+            auto eraseJobSync = new SyncJob<QKeychain::DeletePasswordJob>(QString::fromStdString(storagename));
             eraseJobSync->setKey(QString::fromUtf8(key));
             eraseJobSync->start({});
         }
+        hascached = {};
     }
 
-template<class Layout>
-    template<auto Member>
-    auto Storage<Layout>::load() const
+template <typename Layout, fixed_string storagename>
+void Storage<Layout, storagename>::dosave()
+{
+    ylt::reflection::for_each(cache, [&](auto& member, auto key, auto index)
     {
-        SyncJob<QKeychain::ReadPasswordJob> readJobSync (QString::fromUtf8(storage_name));
+        auto writeJobSync = new SyncJob<QKeychain::WritePasswordJob>(QString::fromStdString(storagename));
+        writeJobSync->setKey(key.data());
+
+        const QByteArray data( reinterpret_cast<char*>(&cache) + ylt::reflection::member_offsets<Layout>[index] , sizeof(member));
+        writeJobSync->setBinaryData(data);
+
+        writeJobSync->start(nullptr);
+    });
+}
+
+template<class Layout, fixed_string storagename>
+    template<auto Member>
+    auto Storage<Layout, storagename>::load() const
+    {
+        const auto &index = ylt::reflection::index_of<Member>();
+        if (hascached[index])
+        {
+            return cache.*Member;
+        }
+
+        SyncJob<QKeychain::ReadPasswordJob> readJobSync (QString::fromStdString(storagename));
+
 
         using Traits = ylt::reflection::internal::member_tratis<decltype(Member)>;
         using FieldT = Traits::value_type;
@@ -142,110 +213,91 @@ template<class Layout>
             }
         });
 
-        return value;  // 返回读取的值
+        //返回前
+        hascached[index] = true;
+        return cache.*Member = value;
     }
 
-template<class Layout>
+template<class Layout, fixed_string storagename>
     template <auto Member, typename T>
-    bool Storage<Layout>::save(const T& value)
+    void Storage<Layout, storagename>::save(T&& value)
     {
-        SyncJob<QKeychain::WritePasswordJob> writeJobSync (QString::fromUtf8(storage_name));
+        //只需要写缓存就好了
+        const auto &index = ylt::reflection::index_of<Member>();
 
-        using Traits = ylt::reflection::internal::member_tratis<decltype(Member)>;
-        static_assert(std::is_same_v<std::decay_t<T>, typename Traits::value_type>,
-                      "value type must match field type");
-
-        auto key = QString::fromUtf8(keys[ylt::reflection::index_of<Member>()]);
-        writeJobSync.setKey(key);
-
-        QByteArray data(reinterpret_cast<const char*>(&value), sizeof(T));
-        writeJobSync.setBinaryData(data);
-
-        bool status;
-        writeJobSync.start([&status](QKeychain::Job* job)
-        {
-            status = job->error() == QKeychain::NoError;
-        });
-
-        return status;
+        cache.*Member = value;
+        hascached[index] = true;
     }
-
-template <typename Layout>
-Storage<Layout>::~Storage() = default;
 
 #else
 #include <filesystem>
 
-template<class Layout>
-    bool Storage<Layout>::exist() const
+template<class Layout, fixed_string storagename>
+    bool Storage<Layout, storagename>::exist() const
     {
-        return std::filesystem::exists(std::filesystem::path(".")/storage_name);
+        return std::filesystem::exists(std::string(storagename));
     }
 
-template<class Layout>
-    Storage<Layout>::operator bool() const
+template<class Layout, fixed_string storagename>
+    Storage<Layout, storagename>::operator bool() const
     {
         if (!exist())
         {
-            _file.open(storage_name, std::ios::binary | std::ios::out | std::ios::in | std::ios::trunc);
+            _file.open(storagename, std::ios::binary | std::ios::out | std::ios::in | std::ios::trunc);
         }
-        else if (!_file.is_open())
+        else if (! _file.is_open())
         {
-            _file.open(storage_name, std::ios::binary | std::ios::out | std::ios::in);
+            _file.open(storagename, std::ios::binary | std::ios::out | std::ios::in);
+            return _file.is_open() && _file.good();
         }
-        return _file.is_open() && _file.good();
+        return _file.good();
     }
 
-template<class Layout>
+template<class Layout, fixed_string storagename>
     template <auto Member>
-    // ylt::reflection::internal::member_tratis<decltype(Member)>::value_type
-    auto Storage<Layout>::load() const {
-        std::lock_guard<std::mutex> lock(_mtx);
+    auto Storage<Layout, storagename>::load() const {
         using Traits = ylt::reflection::internal::member_tratis<decltype(Member)>;
         using Owner =  Traits::owner_type;
         using FieldT = Traits::value_type;
 
-        constexpr size_t idx = ylt::reflection::index_of<Member>();
-        size_t off = ylt::reflection::member_offsets<Owner>[idx];
+        constexpr auto idx = ylt::reflection::index_of<Member>();
 
-        FieldT value{};
-        _file.seekg(off);
+        if (hascached[idx])
+            return cache.*Member;
 
+        FieldT value;
+        _file.seekg(ylt::reflection::member_offsets<Owner>[idx]);
         _file.read(reinterpret_cast<char*>(&value), sizeof(FieldT));
 
         return value;
     }
 
-template<class Layout>
+template<class Layout, fixed_string storagename>
     template <auto Member, typename T>
-    bool Storage<Layout>::save(const T& value) {
+    void Storage<Layout, storagename>::save(T&& value) {
         using Traits = ylt::reflection::internal::member_tratis<decltype(Member)>;
-        using Owner = Traits::owner_type;
-        using FieldT = Traits::value_type;
-
-        static_assert(std::is_same_v<std::decay_t<T>, FieldT>,
+        static_assert(std::is_same_v<std::decay_t<T>, typename Traits::value_type>,
                       "value type must match field type");
 
-        // 通过成员指针求索引
-        constexpr size_t idx = ylt::reflection::index_of<Member>();
-        size_t off = ylt::reflection::member_offsets<Owner>[idx];
-
-        _file.seekp(off);
-        auto size = sizeof(FieldT);
-        _file.write(reinterpret_cast<const char*>(&value), sizeof(FieldT));
-        return _file.good();
+        //我居然吧按偏移写改了那我最初写这个类的意义何在
+        //算了天有不测风云照用吧 --25.10.11
+        cache.*Member = std::forward<T>(value);
+        hascached[ylt::reflection::index_of<Member>()] = true;
     }
 
-template<class Layout>
-    void Storage<Layout>::clear()
+template<class Layout, fixed_string storagename>
+    void Storage<Layout, storagename>::dosave()
+    {
+        //question: std::file保证内存字节序和文件字节序一样吗？
+        _file.seekp(0);
+        _file.write((char*)&cache, sizeof(cache));
+    }
+
+template<class Layout, fixed_string storagename>
+    void Storage<Layout, storagename>::clear()
     {
         _file.close();
-        std::filesystem::remove("./_file");
+        std::filesystem::remove(std::string(storagename));
     }
 
-template<class Layout>
-    Storage<Layout>::~Storage()
-    {
-        _file.close();
-    }
 #endif
